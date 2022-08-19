0d3b54d84de73ba12741abab94817803dba1998f
Amandine Hourewitz <a.hourewitz@gmail.com>
2022-08-20 00:16:51 +0200

    - Newsletter Form

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-function greeter(name) {
-    alert("Coucou " + name)
+function OpenModal() {
+    let element = document.getElementById('overlay')
+    element.style.display = 'block'
+    let popup_element = document.getElementById('popup')
+    popup_element.style.animation = 'popup_move_in 1s'
+}
+function CloseModal() {
+    let popup_element = document.getElementById('popup')
+    popup_element.style.animation = 'popup_move_out 1s'
+    function delay(time) {
+        return new Promise(resolve => setTimeout(resolve, time));
+    }
+    delay(800).then(() => {
+        let element = document.getElementById('overlay')
+        element.style.display = 'none';
+    });
+
 }
